bf321f4f5bae13400416c432e410894df83cecbd
ykk-y <ykk_y@naver.com>
2022-01-06 20:22:59 +0900

    11-2

--- chapter11.py
+++ chapter11.py
@@ -2,7 +2,25 @@
 1: 정렬, 문제 잘 읽기('최대' 몇개의 그룹임. 최대 사람 수가 아님..)
     ... 어떻게 비교하지... 카운트 변수 하나만으로는 힘들겠다
     =>갯수, 해당 숫자, result 에 대한 변수를 정해놓아야 비교가 가능할 것 같다.
+
+2. for문에서 처음 변수를 어떻게할지 고민했는데 그냥 for문 밖에서 한번 해주면 되는것 같다.
 """
+#02984
+def solution_2():
+    x = input()
+    result=int(x[0]) #맨 처음은..?
+    for i in range(1,len(x)):
+        n = int(x[i])
+        if result==0 or result==1: #0이나 1이면 더하기, 나머지는 곱하기
+            result += n
+        else :
+            result *= n
+    print(result)
+
+
+solution_2()
+
+
 #5
 #2 3 1 2 2
 
@@ -22,4 +40,4 @@ def solution_1():
     print(result)
 
 
-solution_1()
+#solution_1()
