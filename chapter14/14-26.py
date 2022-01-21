4d1303d32fe953d1fdf5295c0601c54a7781c412
ykk-y <ykk_y@naver.com>
2022-01-21 22:56:14 +0900

    heapq.heappush(x,원소), heapq.heappop(x), heapq는 작은 순서대로 착착 정리됨

--- chapter14/14-26.py
+++ chapter14/14-26.py
@@ -3,17 +3,36 @@ for를 여러번 쓸수는 없는것 같은데 정렬을
 '''
 import heapq
 
-N=int(input())
-x=[]
-for i in range(N):
-    x.append(int(input()))
-x.sort()
+def solution_2():
+    N = int(input())
+    x = []
+    result=0
+    for i in range(N):
+        heapq.heappush(x,int(input()))
 
-tmp=x[0]
-result=0
-for i in range(1,N):
-    tmp = tmp + x[i]
-    result+=tmp
-print(result)
+    while len(x) != 1:
+        one = heapq.heappop(x)
+        two = heapq.heappop(x)
+        sum = one+two
+        result += sum
+        heapq.heappush(x,sum)
+    print(result)
+
+
+
+def solution_1():
+    N = int(input())
+    x = []
+    for i in range(N):
+        x.append(int(input()))
+    x.sort()
+
+    tmp = x[0]
+    result = 0
+    for i in range(1, N):
+        tmp = tmp + x[i]
+        result += tmp
+    print(result)
+
 
 
