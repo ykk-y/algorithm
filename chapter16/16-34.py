6d755e582e24a319dc24ea8ce2752f69d584687e
ykk-y <ykk_y@naver.com>
2022-02-12 11:06:01 +0900

    16-34

--- chapter16/16-34.py
+++ chapter16/16-34.py
@@ -1,18 +1,16 @@
+'''
+while을 써서 큰값을 만날 때 조건을 걸어주었는데
+for로 전체를 다 보아야 했음
+dp여서 전체를 다 보면 낭비 아닌가 했는데.. LIS 문제라고 함
+'''
+
 N=int(input())
 x=list(map(int,input().split()))
-y=[0]*(N)
-y[0]=1
+y=[1]*(N)
 for i in range(1,N):
-    if x[i-1]>x[i]:
-        y[i]=y[i-1]+1
-    else:
-        j=i-2
-        while x[j]<x[i] and j>=0:
-            j-=1
-        if j==-1:
-            y[i]=1
-        else:
-            y[i]=y[j]+1
+    for j in range(i):
+        if x[j]>x[i]:
+            y[i]=max(y[j]+1,y[i])
 print(N-max(y))
 '''
 7
